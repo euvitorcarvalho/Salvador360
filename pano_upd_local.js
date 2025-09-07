a1f5e81be20f47727db216fc63d6a6783ca98024
pavelpereverzev <pasha004@yandex.ru>
2025-09-07 16:23:40 +0300

    Update 1,1 (adding listeners)
    - Listeners for changing yaw/pitch/zoom on panorama

--- pano_upd_local.js
+++ pano_upd_local.js
@@ -1,1 +1,35 @@
-document.addEventListener("DOMContentLoaded", function(){ pannellum.viewer('panorama', {"type": "equirectangular", "panorama": "image.JPG", "autoLoad": true, "showControls":false, "compass":false})})
+document.addEventListener("DOMContentLoaded", function() {
+    viewer = pannellum.viewer('panorama', {
+        "type": "equirectangular",
+        "panorama": "image.JPG",
+        "autoLoad": true,
+        "showControls": false,
+        "compass": false
+    })
+    let isDragging = false;
+
+    ['mousedown', 'touchstart'].forEach(ev => {
+        viewer.on(ev, () => { isDragging = true; });
+    });
+
+    ['mouseup', 'touchend', 'touchcancel'].forEach(ev => {
+        viewer.on(ev, () => { isDragging = false; });
+    });
+
+    function update() {
+        if (isDragging) {
+            const yaw = viewer.getYaw().toFixed(2);
+            const pitch = viewer.getPitch().toFixed(2);
+            const zoom = viewer.getHfov().toFixed(2);
+            console.log(`{"yaw": ${yaw}, "pitch":${pitch}, "zoom":${zoom}}`)
+        }
+        requestAnimationFrame(update);
+    }
+    update();
+    viewer.on('zoomchange', hfov => {
+        const yaw = viewer.getYaw().toFixed(2);
+        const pitch = viewer.getPitch().toFixed(2);
+        const zoom = viewer.getHfov().toFixed(2);
+        console.log(`{"yaw": ${yaw}, "pitch":${pitch}, "zoom":${zoom}}`)
+    });
+})
