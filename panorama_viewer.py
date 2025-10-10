7382552abef2264ff4b0142e7f031c9c0a6eb176
euvitorcarvalho <familiacarvalhofeliz@gmail.com>
2025-10-10 20:08:18 -0300

    name change

--- panorama_viewer.py
+++ panorama_viewer.py
@@ -69,7 +69,7 @@ class PanoramaViewer:
 
         # Declare instance attributes
         self.actions = []
-        self.menu = self.tr(u'Salvador 360')
+        self.menu = self.tr(u'Salvador 360°')
 
         # Check if plugin was started the first time in current QGIS session
         # Must be set in initGui() to survive plugin reloads
